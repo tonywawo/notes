0266a6e44348b1f10af776aee63a684ac13b7b83
daqing <zhayunqin@foxmail.com>
2015-11-26 21:46:13 +0800

    first commit

---
+++ flasknote.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#virtualenv venv
+. venv/bin/activate
+
+#git clone https://github.com/miguelgrinberg/flasky.git
+#pip install flask-bootstrap
+#pip install flask-script
+#python hello.py runserver
+#/etc/init.d/iptables stop
+#/etc/init.d/iptables restart
